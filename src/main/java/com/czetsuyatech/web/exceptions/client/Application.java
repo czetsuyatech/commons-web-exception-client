0f2bd80f2d9135e23ab8f4d2fff20a795015234d
Edward Legaspi <Ed.Legaspi@contracted.pmi.com>
2025-10-19 18:41:38 +0800

    chore: use config to enable the lib

--- src/main/java/com/czetsuyatech/web/exceptions/client/Application.java
+++ src/main/java/com/czetsuyatech/web/exceptions/client/Application.java
@@ -3,9 +3,14 @@ package com.czetsuyatech.web.exceptions.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @EnableConfigurationProperties
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = DataSourceAutoConfiguration.class,
+    excludeName = {
+        "org.springframework.cloud.autoconfigure.RefreshAutoConfiguration"
+    })
 public class Application {
 
   public static void main(String[] args) {
